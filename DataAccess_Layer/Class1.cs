c8d7c4888fd7b131004946559e84af46a762aead
AlioM46 <aliomaralsloom@gmail.com>
2024-10-17 02:39:10 +0300

    3rd try

---
+++ DataAccess_Layer/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace DataAccess_Layer
+{
+    public class clsDataAccessSettings
+    {
+        public static string connectionString { get; set; }
+
+        public static bool IsValidCredentials(string username, string password)
+        {
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                return true;
+                // Continue Here.
+            }
+
+        }
+
+    }
+}
